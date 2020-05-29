c69da3797c95d0854a648f9774b4811e709100a6
Sebastian <61376632+nrgsille76@users.noreply.github.com>
2020-05-29 18:55:41 +0200

    Fixed Alpha Flag
    check if node socked is alpha

--- io_scene_3ds/export_3ds.py
+++ io_scene_3ds/export_3ds.py
@@ -529,10 +529,12 @@ def make_material_texture_chunk(chunk_id, texslots):
         mat_sub_file = _3ds_chunk(MATMAPFILE)
         mat_sub_file.add_variable("mapfile", _3ds_string(sane_name(filename)))
         mat_sub.add_subchunk(mat_sub_file)
+        for link in texslot.socket_dst.links:
+            socket = link.from_socket
 
         maptile = 0
         
-        if texslot.node_image.outputs['Alpha'].is_linked:
+        if socket.identifier == 'Alpha':
             maptile |= 0x40
         # no perfect mapping for mirror modes - 3DS only has uniform mirror w. repeat=2
         elif texslot.extension == 'EXTEND':
