507dd3ccc3f523915149d9f690fab2fc88e6761e
Sebastian <61376632+nrgsille76@users.noreply.github.com>
2020-08-19 00:28:08 +0200

    Export mapping colors

--- io_scene_3ds/export_3ds.py
+++ io_scene_3ds/export_3ds.py
@@ -610,13 +610,16 @@ def make_material_texture_chunk(chunk_id, texslots, pct):
         mat_sub_angle.add_variable("mapangle", _3ds_float(round(texslot.rotation[2],6)))
         mat_sub.add_subchunk(mat_sub_angle)
 
-        if texslot.socket_dst.identifier in {'Base Color', 'Specular'}:
+        if tex.socket_dst.identifier == 'Base Color':
             rgb = _3ds_chunk(MAP_COL1) # Add tint color
-            base = texslot.owner_shader.material.diffuse_color[:3]
-            spec = texslot.owner_shader.material.specular_color[:]
-            rgb.add_variable("mapcolor", _3ds_rgb_color(spec if texslot.socket_dst.identifier == 'Specular' else base))
-            mat_sub.add_subchunk(rgb)
-                     
+            rgb.add_variable("mapcolor", _3ds_rgb_color(tex.socket_dst.default_value[:3]))
+            ma_sub.add_subchunk(rgb)
+
+        if tex.socket_dst.identifier == 'Specular':
+            rgb = _3ds_chunk(MAP_COL1) # Add specular tint
+            rgb.add_variable("mapcolor", _3ds_rgb_color(tex.owner_shader.material.specular_color[:]))
+            ma_sub.add_subchunk(rgb)
+
     # store all textures for this mapto in order. This at least is what
     # the 3DS exporter did so far, afaik most readers will just skip
     # over 2nd textures.
